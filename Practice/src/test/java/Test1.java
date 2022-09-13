ac07bd5d9db25d3a8d670b2e250f0ce461eb5a24
HULK <HULK@DESKTOP-1QPN6TN>
2022-09-13 19:39:43 +0530

    JMeter Prac

---
+++ Practice/src/test/java/Test1.java
@@ -0,0 +1,26 @@
+import org.testng.annotations.Test;
+
+public class Test1 {
+
+	@Test
+	public void A()
+	{
+		
+		System.out.println("Its A");
+	}
+
+	@Test
+	public void B()
+	{
+		
+		System.out.println("Its B");
+	}
+	
+	@Test
+	public void C()
+	{
+		
+		System.out.println("Its C");
+	}
+
+}
